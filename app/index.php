7ef3ba578732649588c9eca47b6e12a7c87cbe57
Ashley <grigsby.ashley@gmail.com>
2017-10-30 12:13:01 -0400

    Basic CSS styling

--- app/index.php
+++ app/index.php
@@ -2,9 +2,48 @@
 <html>
 <head>
 	<title>Herobook - Facebook for Heros</title>
+    <link href="https://fonts.googleapis.com/css?family=Oswald|Roboto" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="/app/style.css">
 </head>
 <body>
-	<h1>Herobook</h1>
+<div class="container">
+	<header role="banner">
+    <h1>Header (photo)</h1>
+</header>
+   
+<nav id="nav" role="navigation">    
+    <ul>
+        <li>
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a href="#">Friends</a>
+        </li>
+        <li>
+            <a href="#">Enemies</a>
+        </li>
+    </ul>
+</nav>
 
+   
+<aside>
+    <h3>Profile Picture</h3>
+    <p><i>Quote</i></p>
+</aside>
+
+   
+<section role="main">
+    <article>
+        <h2>About</h2>
+        <p>Curabitur orci lacus, auctor ut facilisis nec, ultricies quis nibh. Phasellus id diam sollicitudin, malesuada turpis id, gravida erat. Maecenas placerat elit vel hendrerit convallis. Sed in mauris ut justo vulputate viverra feugiat ac dui. Fusce feugiat arcu in vehicula vehicula. Donec varius justo at nulla aliquet volutpat.</p> 
+        
+    </article>
+</section>
+
+<footer>
+    <h3>Footer</h3>
+</footer>
+
+</div>
 </body>
 </html>
